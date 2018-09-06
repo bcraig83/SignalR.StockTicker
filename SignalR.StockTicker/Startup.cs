d82a27f33d214df299adc6a8c23efe4817b78f2a
Ben Craig <ben.craig@sita.aero>
2018-09-06 14:41:54 +0100

    Add working signal r example project.

---
+++ SignalR.StockTicker/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SignalR.StockTicker;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SignalR.StockTicker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
